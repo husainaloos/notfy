fbafb0e2d76dd3857714f0c5836d421e5576ea73
Husain Alshehhi <husainaloos@users.noreply.github.com>
2018-12-13 10:52:54 -0600

    another update

--- email/postgres.go
+++ email/postgres.go
@@ -38,7 +38,7 @@ func (s *PostgresStorage) insert(ctx context.Context, e Email) (Email, error) {
 	}
 	emailID := 0
 	query := `INSERT INTO notfy.email ("from", "to", cc, bcc, subject, body, status_events) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING email_id`
-	err = s.db.QueryRowContext(ctx, query, e.StringFrom(), pq.Array(e.StringTo()), pq.Array(e.StringCC()), pq.Array(e.StringBCC()), e.Subject(), e.Body(), string(bin)).Scan(&emailID)
+	err = s.db.QueryRowContext(ctx, query, e.StringFrom(), pq.Array(e.StringTo()), pq.Array(e.StringCC()), pq.Array(e.StringBCC()), e.Subject(), e.Body(), bin).Scan(&emailID)
 	if err != nil {
 		return Email{}, err
 	}
@@ -47,7 +47,7 @@ func (s *PostgresStorage) insert(ctx context.Context, e Email) (Email, error) {
 }
 
 func (s *PostgresStorage) get(ctx context.Context, id int) (Email, bool, error) {
-	query := `SELECT (email_id, "from", "to", cc, bcc, subject, body, status_events) FROM notfy.email WHERE email_id = $1`
+	query := `SELECT email_id, "from", "to", cc, bcc, subject, body, status_events FROM notfy.email WHERE email_id = $1`
 	rows, err := s.db.QueryContext(ctx, query, id)
 	if err != nil {
 		return Email{}, true, err
@@ -55,34 +55,18 @@ func (s *PostgresStorage) get(ctx context.Context, id int) (Email, bool, error) 
 	defer rows.Close()
 	for rows.Next() {
 		var id int
-		var from, subject, body, dbStatusEvent string
-		var pqTo, pqCC, pqBCC []sql.NullString
-		if err := rows.Scan(&id, &from, pq.Array(&pqTo), pq.Array(&pqCC), pq.Array(&pqBCC), &subject, &body, &dbStatusEvent); err != nil {
-			c, _ := rows.Columns()
-			return Email{}, true, fmt.Errorf("cannot scan row: %v. columns: %v", err, c)
+		var from, subject, body string
+		var dbStatusEvent []byte
+		var pqTo, pqCC, pqBCC pq.StringArray
+		if err := rows.Scan(&id, &from, &pqTo, &pqCC, &pqBCC, &subject, &body, &dbStatusEvent); err != nil {
+			return Email{}, true, fmt.Errorf("cannot scan row: %v", err)
 		}
 
-		var to, cc, bcc []string
-		for _, r := range pqTo {
-			var str string
-			r.Scan(&str)
-			to = append(to, str)
-		}
-		for _, r := range pqCC {
-			var str string
-			r.Scan(&str)
-			cc = append(cc, str)
-		}
-		for _, r := range pqBCC {
-			var str string
-			r.Scan(&str)
-			bcc = append(bcc, str)
-		}
 		se := []pgStatusEvent{}
-		if err := json.Unmarshal([]byte(dbStatusEvent), &se); err != nil {
+		if err := json.Unmarshal(dbStatusEvent, &se); err != nil {
 			return Email{}, true, fmt.Errorf("cannot json.Unmarshal status_events: %v and values is %s and from %s", err, dbStatusEvent, from)
 		}
-		e, err := New(id, from, to, cc, bcc, subject, body)
+		e, err := New(id, from, pqTo, pqCC, pqBCC, subject, body)
 		if err != nil {
 			return Email{}, true, fmt.Errorf("cannot build email: %v", err)
 		}
@@ -92,4 +76,34 @@ func (s *PostgresStorage) get(ctx context.Context, id int) (Email, bool, error) 
 		return e, true, nil
 	}
 	return Email{}, false, nil
+}
+
+func (s *PostgresStorage) update(ctx context.Context, e Email) (Email, bool, error) {
+	var se []pgStatusEvent
+	for _, v := range e.StatusHistory() {
+		se = append(se, pgStatusEvent{
+			Status: int32(v.Status()),
+			At:     v.At(),
+		})
+	}
+
+	bin, err := json.Marshal(se)
+	if err != nil {
+		return Email{}, true, fmt.Errorf("unable to marshal status_events: %v", err)
+	}
+
+	query := `UPDATE notfy.email SET "from" = $1, "to" = $2, cc = $3, bcc = $4, subject = $5, body = $6, status_events = $7 WHERE email_id = $8`
+	result, err := s.db.ExecContext(ctx, query, e.StringFrom(), pq.Array(e.StringTo()), pq.Array(e.StringCC()), pq.Array(e.StringBCC()), e.Subject(), e.Body(), bin, e.ID())
+	if err != nil {
+		return Email{}, true, err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return Email{}, true, fmt.Errorf("cannot get the number of rows affected: %v", err)
+	}
+	if rows == 0 {
+		return Email{}, false, nil
+	}
+
+	return e, true, nil
 }
--- email/postgres_test.go
+++ email/postgres_test.go
@@ -2,25 +2,55 @@ package email
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestPostgressStorageInsert(t *testing.T) {
 	e, _ := New(0, "myself@myself.com", []string{"to@to.com"}, []string{"cc@cc.com", "cc2@cc.com"}, []string{}, "subject", "body")
-	e.AddStatusEvent(MakeStatusEvent(Created, time.Now()))
-	e.AddStatusEvent(MakeStatusEvent(Queued, time.Now()))
+	t1, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	t2, _ := time.Parse(time.RFC3339, "2007-02-02T15:04:05Z")
+	e.AddStatusEvent(MakeStatusEvent(Created, t1))
+	e.AddStatusEvent(MakeStatusEvent(Queued, t2))
 	connStr := "postgres://postgres:postgres@localhost/notfy?sslmode=disable"
 	pg, err := NewPostgresStorage(connStr)
 	if err != nil {
 		t.Fatalf("failed to create connection: %v", err)
 	}
-	e, err = pg.insert(context.Background(), e)
+	got, err := pg.insert(context.Background(), e)
 	if err != nil {
 		t.Fatalf("failed to insert email: %v", err)
 	}
-	if e.ID() <= 0 {
+	if got.ID() <= 0 {
 		t.Fatalf("email id is %d, but expected it to be greater than 0", e.ID())
+	}
+	if !reflect.DeepEqual(got.From(), e.From()) {
+		t.Fatalf("got %v, but expected %v", got.From(), e.From())
+	}
+
+	if !reflect.DeepEqual(got.To(), e.To()) {
+		t.Fatalf("got %v, but expected %v", got.To(), e.To())
+	}
+
+	if !reflect.DeepEqual(got.CC(), e.CC()) {
+		t.Fatalf("got %v, but expected %v", got.CC(), e.CC())
+	}
+
+	if !reflect.DeepEqual(got.BCC(), e.BCC()) {
+		t.Fatalf("got %v, but expected %v", got.BCC(), e.BCC())
+	}
+
+	if !reflect.DeepEqual(got.Subject(), e.Subject()) {
+		t.Fatalf("got %v, but expected %v", got.Subject(), e.Subject())
+	}
+
+	if !reflect.DeepEqual(got.Body(), e.Body()) {
+		t.Fatalf("got %v, but expected %v", got.Body(), e.Body())
+	}
+
+	if !reflect.DeepEqual(got.StatusHistory(), e.StatusHistory()) {
+		t.Fatalf("got %v, but expected %v", got.StatusHistory(), e.StatusHistory())
 	}
 }
 
@@ -37,5 +67,45 @@ func TestPostgressStorageGet(t *testing.T) {
 	if !ok {
 		t.Fatal("email does not exists, but the email should exists")
 	}
-	t.Fatal(e)
+	expect, _ := New(1, "myself@myself.com", []string{"to@to.com"}, []string{"cc@cc.com", "cc2@cc.com"}, []string{}, "subject", "body")
+	t1, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	t2, _ := time.Parse(time.RFC3339, "2007-02-02T15:04:05Z")
+	expect.AddStatusEvent(MakeStatusEvent(Created, t1))
+	expect.AddStatusEvent(MakeStatusEvent(Queued, t2))
+
+	if !reflect.DeepEqual(e, expect) {
+		t.Fatalf("got %v, but expected %v", e, expect)
+	}
+}
+
+func TestPostgressStorageUpdate(t *testing.T) {
+	connStr := "postgres://postgres:postgres@localhost/notfy?sslmode=disable"
+	pg, err := NewPostgresStorage(connStr)
+	if err != nil {
+		t.Fatalf("failed to create connection: %v", err)
+	}
+	e, _ := New(2, "myself@myself.com", []string{"to@to.com"}, []string{"cc@cc.com", "cc2@cc.com"}, []string{}, "subject", "body")
+	now := time.Now()
+	e.AddStatusEvent(MakeStatusEvent(Created, now))
+	e.AddStatusEvent(MakeStatusEvent(Queued, now))
+
+	got, ok, err := pg.update(context.Background(), e)
+	if err != nil {
+		t.Fatalf("failed to update email: %v", err)
+	}
+	if !ok {
+		t.Fatal("email does not exists, but the email should exists")
+	}
+	if !reflect.DeepEqual(got, e) {
+		t.Fatalf("got %v, but expected %v", got, e)
+	}
+
+	got, ok, err = pg.get(context.Background(), 2)
+	if err != nil || !ok {
+		t.Fatalf("ok=%t, err=%v", ok, err)
+	}
+
+	if !reflect.DeepEqual(got, e) {
+		t.Fatalf("got %v, but expected %v", got, e)
+	}
 }
